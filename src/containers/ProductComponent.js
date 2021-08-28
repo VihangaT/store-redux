4092af5fce18f26d2609ca0cc3020b12732f6ced
vihanga <liyanage.vihanga99@gmail.com>
2021-08-29 02:56:14 +0530

    implementation completed

--- src/containers/ProductComponent.js
+++ src/containers/ProductComponent.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
-
 const ProductComponent = () => {
   const products = useSelector((state) => state.allProducts.products);
   const renderList = products.map((product) => {
